73ea9e0bf636f2b00699794bd2d351c573cf4564
helong <helong.w@gmail.com>
2017-01-25 22:27:21 +0100

    Change GA to class

--- new_structure.py
+++ new_structure.py
@@ -4,6 +4,8 @@ from numpy.random import rand, randint
 from GeneralFunction import *
 from Ship_FCM import *
 import matplotlib.pyplot as plt
+from matplotlib import animation
+from datetime import datetime, timedelta
 
 # INITIATE WEATHER CLASS
 weather_info = WeatherInfo("Metdata_NorthAtlantic_2015-01-01-2015-01-31.mat")
@@ -19,6 +21,15 @@ class state:
         self.bearing = bearing
         self.time = 0.0
         self.speed = 0.0
+        
+        
+class GA_beta:
+    def __init__(self， dep, des, n):
+        X,Y = m.gcpoints(dep[0], dep[1], des[0], des[1], n)
+        x,y = m(X, Y, inverse = True)
+        
+        
+        
         
 def cultime(initial_time, delta_t):
     num = len(delta_t)
@@ -68,11 +79,11 @@ def grade(info, initial_time, delta_t, pop, target):
     return summed / (len(pop) * 1.0)
     
     
-    
+ 
     
 def evolve(info, initial_time, delta_t, pop, target, retain = 0.2, random_select = 0.05, mutate = 0.01):
     graded = [(fitness(info, initial_time, delta_t, x, target), x) for x in pop]
-    graded = [x[1] for x in sorted(graded)]
+    graded = [x[1] for x in sorted(graded, key = lambda tup : tup[0])]
     retain_length = int(len(graded) * retain)
     parents = graded[:retain_length]
     
@@ -100,58 +111,60 @@ def evolve(info, initial_time, delta_t, pop, target, retain = 0.2, random_select
             male = parents[male]
             female = parents[female]
             half = len(male) / 2
-            child = male[:half] + female[half:]
+            child = male[:half].tolist() + female[half:].tolist()
+            child = np.array(child)
             children.append(child)
     
     parents.extend(children)
     
     
-    return parents
+    return np.array(parents)
 
-# departure
-p_dep = np.array([-5.0, 49.0])
-# destination
-p_des = np.array([-65.0, 40.0])
-# points in great circle
-X,Y = m.gcpoints(p_dep[0], p_dep[1], p_des[0], p_des[1], 40)
-x, y = m(X,Y, inverse = True)
-dist_bearing = []
-
-for i in range(39):
-    dist_bearing.append(greatcircle_inverse(x[i], y[i], x[i+1], y[i+1]))
-    
-dist_bearing = np.array(dist_bearing)    
-info = np.column_stack((x[:-1], y[:-1], dist_bearing))    
-
-
-
-    
-calm_r = ship_info.weather2fuel(20, 0, 0, 0, -10000, 0, 0, 0.01, 0.01, 0)
-minifuelc = calm_r[3] * greatcircle_inverse(p_dep[0], p_dep[1], p_des[0], p_des[1])[0] / 1.852
-
-
-
-
-initial_time = 0
-velocity = 20
-delta_t = []
-for i in range(39):
-    delta_t.append(info[i,2] / velocity / 1.852)
-delta_t = np.array(delta_t)
-
-
-
-
-## test
-p = population(100, delta_t)
-fitness_history = [grade(info, initial_time, delta_t, p, minifuelc)]
-for i in xrange(100):
-    print i
-    p = evolve(info, initial_time, delta_t, p, minifuelc)
-    fitness_history.append(grade(info, initial_time, delta_t, p, minifuelc))
-
-for datum in fitness_history:
-    print datum
+## departure
+#p_dep = np.array([-5.0, 49.0])
+## destination
+#p_des = np.array([-65.0, 40.0])
+## points in great circle
+#X,Y = m.gcpoints(p_dep[0], p_dep[1], p_des[0], p_des[1], 40)
+#x, y = m(X,Y, inverse = True)
+#dist_bearing = []
+#
+#for i in range(39):
+#    dist_bearing.append(greatcircle_inverse(x[i], y[i], x[i+1], y[i+1]))
+#    
+#dist_bearing = np.array(dist_bearing)    
+#info = np.column_stack((x[:-1], y[:-1], dist_bearing))    
+#
+#
+#
+#    
+#calm_r = ship_info.weather2fuel(20, 0, 0, 0, -10000, 0, 0, 0.01, 0.01, 0)
+#minifuelc = calm_r[3] * greatcircle_inverse(p_dep[0], p_dep[1], p_des[0], p_des[1])[0] / 1.852
+#minifuelc = 0
+#
+#
+#
+#initial_time = 25
+#velocity = 20
+#delta_t = []
+#for i in range(39):
+#    delta_t.append(info[i,2] / velocity / 1.852)
+#delta_t = np.array(delta_t)
+#
+#
+#
+#
+### test
+#
+#p = population(100, delta_t)
+#fitness_history = [grade(info, initial_time, delta_t, p, minifuelc)]
+#for i in xrange(20):
+#    print i
+#    p = evolve(info, initial_time, delta_t, p, minifuelc)
+#    fitness_history.append(grade(info, initial_time, delta_t, p, minifuelc))
+#
+#for datum in fitness_history:
+#    print datum
 
 
 
@@ -167,3 +180,77 @@ for datum in fitness_history:
 #m.drawparallels(np.arange(-90.,120.,5.), labels=[1,0,0,0], fontsize=15)
 #m.drawmeridians(np.arange(-180.,180.,5.), labels=[0,0,0,1], fontsize=15)
 #plt.show()
+
+
+
+
+
+#delta_tnew = delta_t + p[0]
+#speed0 = dist_bearing[:,0] / delta_tnew / 1.852
+#
+#m = Basemap(
+#  projection="merc",
+#  resolution='l',
+#  area_thresh=0.1,
+#  llcrnrlon=-75,
+#  llcrnrlat=35,
+#  urcrnrlon=10,
+#  urcrnrlat=55
+#)
+#
+## animation 
+#fig = plt.figure(figsize=(20, 15))
+#lon, lat = np.meshgrid(weather_info.lon, weather_info.lat)
+#x, y = m(lon, lat)
+#
+#x1, y1 = m(info[:,0], info[:,1])
+#it = cultime(initial_time,delta_tnew)
+#wt = np.linspace(0, (124 - 1) * 6, 124)
+#ani_ind = int(np.argwhere(wt > max(it))[0])
+#anj_ind = int(np.argwhere(wt < min(it))[-1])
+#
+#
+#txt = plt.title('', fontsize = 20)
+#
+#
+#m.drawcoastlines()
+#m.fillcontinents()
+#m.drawparallels(np.arange(-90.,120.,5.), labels=[1,0,0,0], fontsize=15)
+#m.drawmeridians(np.arange(-180.,180.,5.), labels=[0,0,0,1], fontsize=15)
+#
+#def init():
+#    cs = m.pcolormesh(x, y, weather_info.Hs[:,:,0], shading='flat', cmap=plt.cm.jet)
+#    md = weather_info.Met['Time'][0][0]
+#    pd = datetime.fromordinal(int(md)) + timedelta(days=md%1) - timedelta(days = 366)
+#    txt.set_text("GA @ Significant wave height at " + str(pd))
+#    cb = plt.colorbar(cs, orientation ='horizontal', fraction=0.1,shrink=.5, pad=0.05)
+#    cb.ax.tick_params(labelsize=20) 
+#    return [cs]
+#
+#def animate(i):
+#    #
+#    if i < anj_ind+1:
+#        cs = m.pcolormesh(x, y, weather_info.Hs[:,:,i], shading='flat', cmap=plt.cm.jet)
+#        return [cs]
+##    if i == 0:
+##        cs = m.pcolormesh(x, y, weather_info.Hs[:,:,0], shading='flat', cmap=plt.cm.jet)
+##        return [cs]
+#    cs = m.pcolormesh(x, y, weather_info.Hs[:,:,i], shading='flat', cmap=plt.cm.jet)
+#    index = int(max(np.argwhere(it<wt[i])))
+#    m.plot(x1[:index + 1],y1[:index + 1] ,marker='o',markersize = 4, linewidth=3, color='#ff0080')
+#    md = weather_info.Met['Time'][i][0]
+#    pd = datetime.fromordinal(int(md)) + timedelta(days=md%1) - timedelta(days = 366)
+#    txt.set_text("GA @ Significant wave height at " + str(pd))
+#    return [cs]
+#
+#anim = animation.FuncAnimation(fig, animate, init_func=init, frames= ani_ind+1, interval=1000, blit=True)
+#mywriter = animation.FFMpegWriter(fps=1)
+#
+#anim.save('GA.mp4', writer=mywriter)
+
+
+
+
+
+
+
